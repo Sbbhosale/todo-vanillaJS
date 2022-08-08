d3064c45999266cee0c243f5ab82be35308e8f69
Sbbhosale <sb.bhosaleshubham@gmail.com>
2022-08-07 20:17:54 -0700

    feat : completed the add to list functionality

--- todo.js
+++ todo.js
@@ -2,8 +2,18 @@ let userInput = document.querySelector("#input");
 let addBtn = document.querySelector("#btn");
 let outputSection = document.querySelector("#todosItems");
 
-function inputHandler(){
-    let userValue = userInput.value;
-    outputSection.innerHTML += `<li>Item ${userValue}</li>`
+function inputHandler(){    
+    let todoItem = document.createElement("li");
+    let todoItemValueSection = document.createElement('div');
+    todoItemValueSection.innerText = userInput.value;
+    let todoItemCheckBtn = document.createElement('button');
+    todoItemCheckBtn.innerText = "Check";
+    let todoItemDeleteBtn = document.createElement('button');
+    todoItemDeleteBtn.innerText = "Delete";
+    todoItem.append(todoItemValueSection);
+    todoItem.append(todoItemCheckBtn);
+    todoItem.append(todoItemDeleteBtn);
+    outputSection.append(todoItem);
+    userInput.value = "";
 }
 addBtn.addEventListener('click', inputHandler)
