c9c1e9a3a560978abeed0a05613b29a5cd56d403
Sbbhosale <sb.bhosaleshubham@gmail.com>
2022-08-14 21:44:51 -0700

    feat : empty input handling code updated

--- todo.js
+++ todo.js
@@ -1,50 +1,54 @@
-let userInput = document.querySelector("#input");
-let addBtn = document.querySelector("#btn--add");
+let userInput = document.querySelector("#userInputContainer__input");
+let addBtn = document.querySelector("#userInputContainer__btn--add");
 let outputSection = document.querySelector("#todoItemsContainer");
 
 
-function inputHandler(){    
-    console.log("Hello");
-// create li item
-    let todoItem = document.createElement("li");
-    todoItem.classList.add('todoItemContainer__item');
+function inputHandler() {
 
-// creating user value showing div in li item
-    let todoItemValueSection = document.createElement('div');
-    todoItem.classList.add('todoItemContainer__item__userValueContainer');
-    todoItemValueSection.innerText = userInput.value;
+    if(userInput.value !== ""){
+        // create li item
+        let todoItem = document.createElement("li");
+        todoItem.classList.add('todoItemContainer__item');
 
-// creating btn div in li item
-    let todoItemBtnSection = document.createElement('div');
-    todoItem.classList.add('todoItemContainer__item__btn');
+    // creating user value showing div in li item
+        let todoItemValueSection = document.createElement('div');
+        todoItem.classList.add('todoItemContainer__item__userValueContainer');
+        todoItemValueSection.innerText = userInput.value;
 
-// creating check btn 
-    let todoItemCheckBtn = document.createElement('button');
-    todoItemCheckBtn.classList.add('btn');
-    todoItemCheckBtn.classList.add('todoItemContainer__item__btn--checked');
-    todoItemCheckBtn.innerText = "Check";
+    // creating btn div in li item
+        let todoItemBtnSection = document.createElement('div');
+        todoItem.classList.add('todoItemContainer__item__btn');
 
-// creating delete btn
-    let todoItemDeleteBtn = document.createElement('button');
-    todoItemDeleteBtn.classList.add('btn');
-    todoItemDeleteBtn.classList.add('todoItemContainer__item__btn--remove');
-    todoItemDeleteBtn.innerText = "Delete";
+    // creating check btn 
+        let todoItemCheckBtn = document.createElement('button');
+        todoItemCheckBtn.classList.add('btn');
+        todoItemCheckBtn.classList.add('todoItemContainer__item__btn--checked');
+        todoItemCheckBtn.innerHTML = '<i class="fa fa-lg fa-check" aria-hidden="true"></i>';
 
-// appending check and delete buttons in btn div
-    todoItemBtnSection.append(todoItemCheckBtn);
-    todoItemBtnSection.append(todoItemDeleteBtn);
+    // creating delete btn
+        let todoItemDeleteBtn = document.createElement('button');
+        todoItemDeleteBtn.classList.add('btn');
+        todoItemDeleteBtn.classList.add('todoItemContainer__item__btn--remove');
+        todoItemDeleteBtn.innerHTML = '<i class="fa fa-lg fa-trash" aria-hidden="true"></i>';
 
-// appending user value showing div in li
-    todoItem.append(todoItemValueSection);
 
-// appending btn div in li    
-    todoItem.append(todoItemBtnSection);
+    // appending check and delete buttons in btn div
+        todoItemBtnSection.append(todoItemCheckBtn);
+        todoItemBtnSection.append(todoItemDeleteBtn);
 
-// appending li item in ul    
-    outputSection.append(todoItem);
+    // appending user value showing div in li
+        todoItem.append(todoItemValueSection);
 
-// clearing user input section
-    userInput.value = "";
+    // appending btn div in li    
+        todoItem.append(todoItemBtnSection);
+
+    // appending li item in ul    
+        outputSection.append(todoItem);
+
+    // clearing user input section
+        userInput.value = "";
+    }
+
 }
 
 
